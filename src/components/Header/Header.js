28cb3ea9022e7b825cd48e488c9dd25d753a648f
SADEK HOSSAIN <shadeks16@gmail.com>
2022-04-04 00:45:49 +0600

    Header Added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg="white" expand="lg">
+        <Container>
+          <Navbar.Brand href="/home">Perfume World</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto py-4">
+              <Link className=" text-decoration-none text-dark me-4" to={'/home'}>HOME</Link>
+              <Link className=" text-decoration-none text-dark me-4"  to={'/reviews'}>REVIEWS</Link>
+              <Link className=" text-decoration-none text-dark me-4"  to={'/dashboard'}>DASHBOARD</Link>
+              <Link className=" text-decoration-none text-dark me-4"  to={'/blogs'}>BLOGS</Link>
+              <Link className=" text-decoration-none text-dark me-4"  to={'/about'}>ABOUT</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import './Home.css';
+const Home = () => {
+  return (
+    <div>
+      <Container>
+        <Row className="g-4">
+          <Col sm={12} md={8}  className="left-banner">
+              <div>
+                  <h1 className="fw-bold">Fragrance of the Mountains</h1>
+                  <h2 className="fw-bold ">Base Camp For Men</h2>
+                  <p>We may live in cities but our hearts belong to the mountains. Experience the magic of the cool mountain air in Base Camp. Loaded with 3x more fragrance than a deodorant and no gas, this cologne will make sure you smell great all day.</p>
+                  <button className="btn btn-info px-3 py-2 fs-5">Live Demo</button>
+              </div>
+          </Col>
+          <Col sm={12} md={4}>
+              <img src="https://rukminim2.flixcart.com/image/416/416/ku4ezrk0/perfume/e/d/7/cologne-spray-base-camp-perfume-ustraa-men-original-imag7bgrnpbn57dh.jpeg?q=70" alt="" />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
